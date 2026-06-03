a5ea6d6c3e3ee9577b6cd12ada71e624d60a1a0a
egormashtakov <egor190707e@gmail.com>
2026-06-03 17:00:43 +0300

    Implemented the `Recipe` class (subtask 1.2)

--- classes.py
+++ classes.py
@@ -29,3 +29,39 @@ class Ingredient:
             return False
         return self.name == other.name and self.unit == other.unit
 
+
+class Recipe:
+    def __init__(self, title: str, ingredients: list[Ingredient]) -> None:
+        self.title = title
+        self.ingredients = []
+        for ingredient in ingredients:
+            self.add_ingredient(ingredient)
+
+    def add_ingredient(self, ingredient: Ingredient) -> None:
+        for item in self.ingredients:
+            if item == ingredient:
+                item.quantity += ingredient.quantity
+                return
+
+        self.ingredients.append(Ingredient(ingredient.name, ingredient.quantity, ingredient.unit))
+
+    @staticmethod
+    def is_valid_ratio(ratio: int | float) -> bool:
+        if not isinstance(ratio, (int, float)):
+            return False
+        return ratio > .0
+
+    def scale(self, ratio: float):
+        if not self.is_valid_ratio(ratio):
+            raise ValueError("Ratio must be positive number")
+
+        scaled_ingredients = []
+        for ingredient in self.ingredients:
+            scaled_ingredients.append(Ingredient(ingredient.name, ingredient.quantity * ratio, ingredient.unit))
+        return Recipe(self.title, scaled_ingredients)
+
+    def __len__(self):
+        return len(self.ingredients)
+
+    def __str__(self):
+        return self.title + "\n" + "\n".join(str(ingredient) for ingredient in self.ingredients)
